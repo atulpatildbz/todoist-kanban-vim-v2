e46ce75f386f32337c54f6f713d8a7f2e7f09353
Atul Patil <atulpatildbz@gmail.com>
2025-02-01 13:44:58 +0530

    add compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+const ReactCompilerConfig = {
+  target: '18'
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+    tailwindcss(),
+  ],
+  optimizeDeps: {
+    include: ['react/jsx-runtime'],
+  },
 });
